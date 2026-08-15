f3fd17906852552d0112507ff7d9cd004488d141
morningstar <sitorus1255@gmail.com>
2026-08-15 17:35:39 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 // firebase-config.js - Konfigurasi & Autentikasi Firebase Ruang Tryout
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_9XPLuFNoxRtPd8nrtbqHWL9wd9TwmhE",
+  apiKey: "AIzaSyC_9XPLuFNoxRtPd8nrtbqHML9wd9TwmhE",
   authDomain: "ruang-tryout-b6624.firebaseapp.com",
   projectId: "ruang-tryout-b6624",
   storageBucket: "ruang-tryout-b6624.firebasestorage.app",
